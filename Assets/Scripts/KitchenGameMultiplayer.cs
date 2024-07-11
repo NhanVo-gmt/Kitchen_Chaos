94f9d0a05a3aceac1bde594fcb9d4b301071d8db
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-12 00:54:13 +0700

    update prefab and scene

--- Assets/Scripts/KitchenGameMultiplayer.cs
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -5,5 +6,22 @@ using UnityEngine;
 
 public class KitchenGameMultiplayer : NetworkBehaviour
 {
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+
+        NetworkObject kitchenNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
+        kitchenNetworkObject.Spawn(true);
+
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+        
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+    }
     
 }
