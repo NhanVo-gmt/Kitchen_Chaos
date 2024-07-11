699ffad065cc6407e14062832a7bf53f0fd448e3
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-11 17:14:24 +0700

    change delivery manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Player : NetworkBehaviour, IKitchenObjectParent {
+public class Player : NetworkBehaviour, IKitchenObjectParent
+{
+    public static event EventHandler OnAnyPlayerSpawned;
+    public static event EventHandler OnAnyPickedSomething;
+    
+    public static void ResetStaticData() {
+        OnAnyPlayerSpawned = null;
+    }
 
-
-    // public static Player Instance { get; private set; }
-
-
-
+    public static Player LocalInstance { get; private set; }
+    
     public event EventHandler OnPickedSomething;
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs {
@@ -28,18 +32,20 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
     private Vector3 lastInteractDir;
     private BaseCounter selectedCounter;
     private KitchenObject kitchenObject;
-
-
-    private void Awake() {
-        // if (Instance != null) {
-        //     Debug.LogError("There is more than one Player instance");
-        // }
-        // Instance = this;
-    }
-
+    
     private void Start() {
         GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
         GameInput.Instance.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            LocalInstance = this;
+        }
+        
+        OnAnyPlayerSpawned?.Invoke(this, EventArgs.Empty);
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
@@ -158,6 +164,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
 
         if (kitchenObject != null) {
             OnPickedSomething?.Invoke(this, EventArgs.Empty);
+            OnAnyPickedSomething?.Invoke(this, EventArgs.Empty);
         }
     }
 
