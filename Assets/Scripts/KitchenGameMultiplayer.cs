78a04873a57e45812bd7f66ac53767570986b2d4
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-12 01:13:56 +0700

    handle spawning object

--- Assets/Scripts/KitchenGameMultiplayer.cs
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -8,20 +8,41 @@ public class KitchenGameMultiplayer : NetworkBehaviour
 {
     public static KitchenGameMultiplayer Instance { get; private set; }
 
+    [SerializeField] private KitchenObjectListSO kitchenObjectListSo;
+
     private void Awake()
     {
         Instance = this;
     }
     
     public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        SpawnKitchenObjectServerRpc(GetKitchenObjectSOIndex(kitchenObjectSO), kitchenObjectParent.GetNetworkObject());
+    }
 
+    [ServerRpc(RequireOwnership = false)]
+    void SpawnKitchenObjectServerRpc(int kitchenObjectSOIndex, NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        KitchenObjectSO kitchenObjectSO        = GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        Transform       kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+    
         NetworkObject kitchenNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
         kitchenNetworkObject.Spawn(true);
-
+    
         KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-        
+
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent =
+            kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
         kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
     }
-    
+
+    private int GetKitchenObjectSOIndex(KitchenObjectSO kitchenObjectSo)
+    {
+        return kitchenObjectListSo.kitchenObjectSoList.IndexOf(kitchenObjectSo);
+    }
+
+    private KitchenObjectSO GetKitchenObjectSOFromIndex(int index)
+    {
+        return kitchenObjectListSo.kitchenObjectSoList[index];
+    }
 }
