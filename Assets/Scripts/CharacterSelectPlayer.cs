85e1c6a7341d5593001b83721eca65beba54a02c
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-27 11:54:02 +0700

    adding character select scene

---
+++ Assets/Scripts/CharacterSelectPlayer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectPlayer : MonoBehaviour
+{
+    [SerializeField] private int playerIndex;
+    
+    private void Start()
+    {
+        KitchenGameMultiplayer.Instance.OnPlayerDataNetworkListChanged +=
+            KitchenGameMultiplayer_OnPlayerDataNetworkListChanged;
+        
+        UpdatePlayer();
+    }
+    
+    private void KitchenGameMultiplayer_OnPlayerDataNetworkListChanged(object sender, EventArgs e)
+    {
+        UpdatePlayer();
+    }
+
+    void UpdatePlayer()
+    {
+        if (KitchenGameMultiplayer.Instance.IsPlayerIndexConnected(playerIndex))
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
