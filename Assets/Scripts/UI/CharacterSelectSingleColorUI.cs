708e9aada354e16ce7a0a63ead3ffc0b40eb756a
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-27 12:54:45 +0700

    create color prefab for character select

---
+++ Assets/Scripts/UI/CharacterSelectSingleColorUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectSingleColorUI : MonoBehaviour
+{
+    [SerializeField] private int        colorId;
+    [SerializeField] private Image      image;
+    [SerializeField] private GameObject selectedGameObject;
+}
